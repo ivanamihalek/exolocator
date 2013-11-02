81659b9ac452890e733ffeffdd0ac3b7346a1493
Ivana Mihalek <ivana.mihalek@gmail.com>
2013-11-02 15:38:10 +0100

    debugging exon length

--- 08_gene2exon_check.py
+++ 08_gene2exon_check.py
@@ -38,7 +38,7 @@ def inspect (exons, canonical_translation):
         print "canon transl end: ",   exon.canon_transl_end
         print "exon len %5d   total %d " % (exon_len, total_len)
         # print canonical sequence with the newline stuck in every 50 positions
-        print re.sub("(.{50})", "\\1\n", canonical_translation, 0, re.DOTALL)       
+        print re.sub("(.{50})", "\\1\n", canonical_translation)       
 
         # where did the information about translation start and end come at this point?
 
