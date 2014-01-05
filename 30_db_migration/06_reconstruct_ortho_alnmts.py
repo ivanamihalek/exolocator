974533efa4c9a4f198e977c46fe6b5cb9bac0df0
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-01-05 23:51:43 +0100

    enable 15 to use novel exons

--- 30_db_migration/06_reconstruct_ortho_alnmts.py
+++ 30_db_migration/06_reconstruct_ortho_alnmts.py
@@ -689,7 +689,7 @@ def fix_one2many (cursor, ensembl_db_name, cfg, acg, sorted_seq_names, canonical
                 ( float(pep_slice_end-pep_slice_start)/len(output_pep['human']) > 0.3 or 'elephant' in seq_to_fix):
             #print "deleting", seq_to_fix
             print "abort 2: map apparently spread over too many human exons:"
-            print seq_to_fix, " pep slice", pep_slice_start, pep_slice_end, "  len: ", len(output_pep['human'])
+            print seq_to_fix, " pep slice", pep_slice_start, pep_slice_end, "  len: ", len(output_pep['human']), "no human exons:", number_of_human_exons
             del output_pep[seq_to_fix]
             return [output_pep, names_of_exons]
 
