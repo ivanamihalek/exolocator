5b51673e55422faae8aa85ccb30b50f4d6b92650
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-01-05 23:21:42 +0100

    enable 15 to use novel exons

--- el_utils/utils.py
+++ el_utils/utils.py
@@ -616,6 +616,8 @@ def  pairwise_tanimoto (seq1, seq2):
 
     if not common_length: return tanimoto
 
+    print " ** ", l1, l1, similar_length, common_length
+
     if (similar_length > 0.9*l1 ):
         tanimoto = similar_length/l1
     elif (similar_length > 0.9*l2 ):
