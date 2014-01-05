cf352fe00aa9bed9401b5c3bb5cc96fa391bc0bc
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-01-05 18:46:34 +0100

    enable 15 to use novel exons

--- el_utils/map.py
+++ el_utils/map.py
@@ -123,3 +123,142 @@ def map2exon(cursor, ensembl_db_name, map, paralogue=False):
         exon.analysis_id = 1
  
     return exon
+#########################################
+def self_maps (cursor, ensembl_db_name, human_exons):
+
+    maps = []
+    switch_to_db (cursor, ensembl_db_name['homo_sapiens'])
+    # this should fill in the seqs for the coding exons
+    relevant_human_exons = find_relevant_exons (cursor, human_exons) 
+    for he in relevant_human_exons:
+        map = Map()
+        map.species_1    = 'homo_sapiens'
+        map.species_2    = 'homo_sapiens'                
+        map.exon_id_1    = he.exon_id
+        map.exon_id_2    = he.exon_id
+
+        map.exon_known_1 = he.is_known
+        map.exon_known_2 = he.is_known
+        map.similarity   = 1.0
+
+        pepseq = get_exon_pepseq (cursor, he, verbose=True)
+        map.cigar_line   = cigar_line (pepseq, pepseq)
+        map.source       = 'ensembl'
+        maps.append(map)  
+    return maps
+
+
+###################################################################################################
+###################################################################################################
+
+#########################################
+def maps_evaluate (cfg, human_exons, ortho_exons, aligned_seq, exon_positions):
+
+    maps = []
+   
+    if len(aligned_seq.keys()) > 2:
+        print "right now the mapping implemented for two species only"
+        return maps
+
+    for species in aligned_seq.keys():
+        if species == 'homo_sapiens': continue
+        other_species = species
+        break
+
+    min_similarity = cfg.get_value('min_accptbl_exon_sim')
+
+    for human_exon_ct in range(len(human_exons)):
+
+        padded_count_human = "{0:03d}".format(human_exon_ct+1)
+        if ( not  exon_positions['homo_sapiens'].has_key(padded_count_human) ):
+            continue
+        [human_start, human_end] = exon_positions['homo_sapiens'][padded_count_human]
+
+
+        for ortho_exon_ct in range(len(ortho_exons)):
+
+            padded_count_ortho = "{0:03d}".format(ortho_exon_ct+1)
+            if ( not  exon_positions[other_species].has_key(padded_count_ortho) ):
+                continue
+            [other_start, other_end] = exon_positions[other_species][padded_count_ortho]
+
+            if ( overlap (human_start, human_end, other_start, other_end) ):
+                
+                map = Map()
+                map.species_1    = 'homo_sapiens'
+                map.species_2    = other_species
+                
+                map.exon_id_1    = human_exons[human_exon_ct].exon_id
+                map.exon_id_2    = ortho_exons[ortho_exon_ct].exon_id
+
+                map.exon_known_1 = human_exons[human_exon_ct].is_known
+                map.exon_known_2 = ortho_exons[ortho_exon_ct].is_known
+
+                exon_seq_human   = aligned_seq['homo_sapiens'][human_start:human_end].replace('#','-')
+                exon_seq_other   = aligned_seq[other_species][other_start:other_end].replace('#','-')
+                [seq_human, seq_other] = pad_the_alnmt (exon_seq_human,human_start,
+                                                        exon_seq_other, other_start)
+
+                seq = {'human':seq_human, 'other':seq_other}
+                seq = strip_gaps(seq)
+                if not seq:  
+                    c=inspect.currentframe()
+                    print " in %s:%d" % ( c.f_code.co_filename, c.f_lineno)
+                    continue
+
+                map.similarity = pairwise_tanimoto(seq['human'], seq['other'])
+                if map.source == 'usearch':
+                    print "\t", other_species,  map.similarity, min_similarity
+
+                if map.similarity < min_similarity: continue
+
+                ciggy = cigar_line (seq['human'], seq['other'])
+                map.cigar_line = ciggy
+                                                   
+                maps.append(map)                
+
+    return maps
+
+
+#########################################
+def make_maps (cursor, ensembl_db_name, cfg, acg, ortho_species, human_exons, ortho_exons):
+
+    maps = []
+
+    #print "############################## human"
+    switch_to_db(cursor,  ensembl_db_name['homo_sapiens'])
+    relevant_human_exons = find_relevant_exons (cursor, human_exons)
+    #print "##############################", ortho_species
+    switch_to_db(cursor,  ensembl_db_name[ortho_species])
+    relevant_ortho_exons = find_relevant_exons (cursor, ortho_exons)
+
+    human_seq = decorate_and_concatenate (relevant_human_exons)
+    ortho_seq = decorate_and_concatenate (relevant_ortho_exons)
+    
+    if (not human_seq or not ortho_seq):
+        return maps
+    
+    aligned_seq = {}
+    if 0:# python implementation
+        [aligned_seq['homo_sapiens'], aligned_seq[ortho_species]] \
+            = exon_aware_smith_waterman (human_seq, ortho_seq)
+    else: # C implementation
+        [aligned_seq['homo_sapiens'], aligned_seq[ortho_species]] \
+            = smith_waterman_context (human_seq, ortho_seq, -5, -3)
+
+    if (not aligned_seq['homo_sapiens'] or 
+        not aligned_seq[ortho_species]):
+        return []
+
+    # find the positions of the exons in the alignment
+    exon_positions = {}
+    for species, seq in aligned_seq.iteritems():
+        # move B to beginning of each exon sequence
+        seq = moveB(seq)
+        #beginning and end of each exon in the alignment
+        exon_positions[species] = find_exon_positions(seq)
+
+    # fill in the actual map values
+    maps = maps_evaluate (cfg, relevant_human_exons, relevant_ortho_exons, aligned_seq, exon_positions)
+
+    return maps
---
+++ old/17_ortho_exon_map_to_msa.py
@@ -0,0 +1,252 @@
+#!/usr/bin/python
+# make the best alignment we can using the maps
+# we currently have at hand
+
+import MySQLdb, commands, re, sys
+from   el_utils.mysql   import  connect_to_mysql, connect_to_db
+from   el_utils.mysql   import  switch_to_db,  search_db, store_or_update
+from   el_utils.ensembl import  *
+from   el_utils.utils   import  erropen, output_fasta
+from   el_utils.map     import  get_maps, map2exon, Map
+from   el_utils.tree    import  species_sort
+from   el_utils.ncbi    import  taxid2trivial
+
+from   el_utils.almt_cmd_generator import AlignmentCommandGenerator
+from   el_utils.config_reader      import ConfigurationReader
+from   el_utils.special_gene_sets  import *
+from   el_utils.threads import  parallelize
+
+
+from time      import  time
+from Bio       import  SeqIO
+from bitstring import  Bits
+
+verbose = False
+
+#########################################
+def multiple_exon_alnmt(gene_list, db_info):
+
+
+    [local_db, ensembl_db_name] = db_info
+
+
+    if local_db:
+        db     = connect_to_mysql()
+        cfg    = ConfigurationReader()
+        acg    = AlignmentCommandGenerator()
+    else:
+        db     = connect_to_mysql         (user="root", passwd="sqljupitersql", host="jupiter.private.bii", port=3307)
+        cfg    = ConfigurationReader      (user="root", passwd="sqljupitersql", host="jupiter.private.bii", port=3307)
+        acg    = AlignmentCommandGenerator(user="root", passwd="sqljupitersql", host="jupiter.private.bii", port=3307)
+    cursor = db.cursor()
+
+    # find db ids adn common names for each species db
+    [all_species, ensembl_db_name] = get_species (cursor)
+    
+
+    species  = 'homo_sapiens'
+    switch_to_db (cursor,  ensembl_db_name[species])
+    gene_ids = get_gene_ids (cursor, biotype='protein_coding', is_known=1)
+
+    # for each human gene
+    gene_ct = 0
+    tot  = 0
+    ok   = 0
+    no_maps        = 0
+    no_pepseq      = 0
+    no_orthologues = 0
+    min_similarity = cfg.get_value('min_accptbl_exon_sim')
+
+    for gene_id in gene_list:
+
+        start = time()
+        gene_ct += 1
+        if  not gene_ct%10: print gene_ct, "genes out of", len(gene_list)
+
+        switch_to_db (cursor, ensembl_db_name['homo_sapiens'])
+        print gene_ct, len(gene_ids),  gene_id,  gene2stable(cursor, gene_id), get_description (cursor, gene_id)
+
+        # find all human exons we are tracking in the database
+        human_exons = filter (lambda e: e.is_known==1 and e.is_coding and e.covering_exon<0, gene2exon_list(cursor, gene_id))
+
+        for human_exon in human_exons:
+            
+            tot += 1
+            # find all orthologous exons the human exon  maps to
+            maps = get_maps(cursor, ensembl_db_name, human_exon.exon_id, human_exon.is_known)
+            if verbose: 
+                print "\texon no.", tot, " id", human_exon.exon_id,
+                if not maps: 
+                    print " no maps"
+                    print human_exon
+                print 
+            if not maps: 
+                no_maps += 1
+                continue
+
+            # output to fasta:
+            seqname   = "{0}:{1}:{2}".format('homo_sapiens', human_exon.exon_id, human_exon.is_known)
+            switch_to_db (cursor, ensembl_db_name['homo_sapiens'])
+            [exon_seq_id, pepseq, pepseq_transl_start, pepseq_transl_end, 
+             left_flank, right_flank, dna_seq] = get_exon_seqs (cursor, human_exon.exon_id, human_exon.is_known)
+            if (not pepseq):
+                if verbose and  human_exon.is_coding and  human_exon.covering_exon <0: # this should be a master exon
+                    print "no pep seq for",  human_exon.exon_id, "coding ", human_exon.is_coding,
+                    print "canonical: ",  human_exon.is_canonical
+                    print "length of dna ", len(dna_seq)
+                no_pepseq += 1
+                continue
+            headers   = [seqname]
+            sequences = {seqname:pepseq}
+            hassw = False
+            for map in maps:
+
+                switch_to_db (cursor, ensembl_db_name[map.species_2])
+
+                if map.similarity < min_similarity: continue
+                exon    = map2exon(cursor, ensembl_db_name, map)
+                pepseq  = get_exon_pepseq (cursor,exon)
+                if (not pepseq):
+                    continue
+                if  map.source == 'sw_sharp':
+                    exon_known_code = 2
+                    hassw = True
+                elif  map.source == 'usearch':
+                    exon_known_code = 3
+                    hassw = True
+                    print map.species_2, "has usearch exon"
+                else:
+                    exon_known_code = map.exon_known_2
+                seqname = "{0}:{1}:{2}".format(map.species_2, map.exon_id_2, exon_known_code)
+                headers.append(seqname)
+                sequences[seqname] = pepseq
+
+            if (len(headers) <=1 ):
+                if verbose: print "single species in the alignment"
+                no_orthologues += 1
+                continue
+            
+            fasta_fnm = "{0}/{1}.fa".format( cfg.dir_path['scratch'], human_exon.exon_id)
+            output_fasta (fasta_fnm, headers, sequences)
+
+            # align
+            afa_fnm  = "{0}/{1}.afa".format( cfg.dir_path['scratch'], human_exon.exon_id)
+            mafftcmd = acg.generate_mafft_command (fasta_fnm, afa_fnm)
+            #mafftcmd = "muscle -in" + fasta_fnm + " -out" + afa_fnm
+            ret      = commands.getoutput(mafftcmd)
+
+
+            # read in the alignment
+            inf = erropen(afa_fnm, "r")
+            human_seq_seen = False
+            for record in SeqIO.parse(inf, "fasta"):
+                ### store the alignment as bitstring
+                # Generate the bitmap
+                bs         = Bits(bin='0b' + re.sub("[^0]","1", str(record.seq).replace('-','0')))
+                msa_bitmap = bs.tobytes()
+                
+                # Retrieve information on the cognate
+                cognate_species, cognate_exon_id, cognate_exon_known = record.id.split(':')
+                if cognate_exon_known == '2':
+                    source = 'sw_sharp'
+                elif cognate_exon_known == '3':
+                    source = 'usearch'
+                else:
+                    source = 'ensembl'
+                if (cognate_species == 'homo_sapiens'):
+                    human_seq_seen = True
+                cognate_genome_db_id = species2genome_db_id(cursor, cognate_species) # moves the cursor
+                switch_to_db(cursor, ensembl_db_name['homo_sapiens']) # so move it back to homo sapiens
+                # Write the bitmap to the database
+                #if (cognate_species == 'homo_sapiens'):
+                if verbose and (source=='sw_sharp' or source=='usearch'):
+                    print "storing"
+                    print human_exon.exon_id, human_exon.is_known
+                    print cognate_species, cognate_genome_db_id, cognate_exon_id, cognate_exon_known, source
+                    #print MySQLdb.escape_string(msa_bitmap)
+                    if not msa_bitmap:
+                        print "no msa_bitmap"
+                        exit(1)
+                    print
+                store_or_update(cursor, "exon_map",    {"cognate_genome_db_id":cognate_genome_db_id,
+                   "cognate_exon_id":cognate_exon_id   ,"cognate_exon_known"  :cognate_exon_known,
+                   "source": source, "exon_id" :human_exon.exon_id, "exon_known":human_exon.is_known},
+                  {"msa_bitstring":MySQLdb.escape_string(msa_bitmap)})
+
+            ok += 1
+            commands.getoutput("rm "+afa_fnm+" "+fasta_fnm)
+            #print afa_fnm
+            #exit(1)
+
+        if verbose: print " time: %8.3f\n" % (time()-start);
+
+    print "tot: ", tot, "ok: ", ok
+    print "no maps ",   no_pepseq
+    print "no pepseq ", no_pepseq
+    print "no orthologues  ", no_orthologues
+    print
+
+
+#########################################
+def main():
+    
+    no_threads = 12
+    special    = None
+
+
+    if len(sys.argv) > 1 and  len(sys.argv)<3:
+        print "usage: %s <set name> <number of threads> <method>" % sys.argv[0]
+        exit(1)
+    elif len(sys.argv)==3:
+
+        special = sys.argv[1]
+        special = special.lower()
+        if special == 'none': special = None
+
+        no_threads = int(sys.argv[2])
+
+    local_db = False
+
+    if local_db:
+        db  = connect_to_mysql()
+        cfg = ConfigurationReader()
+    else:
+        db  = connect_to_mysql    (user="root", passwd="sqljupitersql", host="jupiter.private.bii", port=3307)
+        cfg = ConfigurationReader (user="root", passwd="sqljupitersql", host="jupiter.private.bii", port=3307)
+    cursor = db.cursor()
+
+    [all_species, ensembl_db_name] = get_species (cursor)
+
+    print "running ", sys.argv[0]
+
+    if special:
+        print "using", special, "set"
+        if special == 'complement':
+            gene_list = get_complement_ids(cursor, ensembl_db_name, cfg)
+        else:
+            gene_list = get_theme_ids (cursor,  ensembl_db_name, cfg, special )
+
+    else:
+        print "using all protein coding genes"
+        switch_to_db (cursor,  ensembl_db_name['homo_sapiens'])
+        gene_list = get_gene_ids (cursor, biotype='protein_coding', is_known=1)
+        
+ 
+
+    cursor.close()
+    db.close()
+
+    parallelize (no_threads, multiple_exon_alnmt, gene_list, [local_db, ensembl_db_name])
+    
+    return True
+
+#########################################
+if __name__ == '__main__':
+    main()
+
+'''
+    #for gene_id in [412667]: #  wls
+    #for gene_id in [378768]: #  p53
+    #for gene_id in [378766]: #  dynein
+'''
+
