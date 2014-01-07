fff10186bc0345b9b717807289f1e159f4598906
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-01-07 15:16:39 +0100

    fusing seqs from differenst scaffolds

--- 30_db_migration/06_reconstruct_ortho_alnmts.py
+++ 30_db_migration/06_reconstruct_ortho_alnmts.py
@@ -1046,7 +1046,11 @@ def fuse_seqs_split_on_scaffolds(output_pep, names_of_exons, ortho_exon_to_human
         # for all pairs of paralogues
         for [para1, para2] in find_pairs (paralogues):
             # do they map to disjoint set of human exons?
+            print para1
             for exon_name in names_of_exons[para1]:
+                print exon_name, ortho_exon_to_human_exon[exon_name]
+            print para2
+            for exon_name in names_of_exons[para2]:
                 print exon_name, ortho_exon_to_human_exon[exon_name]
             #if human_exons_1 & human_exons_2: continue # there is intersection - we move on
             #print "\t", pair, " maps to disjoint set of human exons"
