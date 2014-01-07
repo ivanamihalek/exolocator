272230ef93f3b125f9a58ba6cf82e057c54e2d25
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-01-07 22:45:48 +0100

    fusing seqs from differenst scaffolds

--- 30_db_migration/06_reconstruct_ortho_alnmts.py
+++ 30_db_migration/06_reconstruct_ortho_alnmts.py
@@ -1125,7 +1125,7 @@ def fuse_seqs_split_on_scaffolds(output_pep, names_of_exons, ortho_exon_to_human
             # make sure we have all exons listed correctly under the new name - we'll need them to expand dna
             new_exon_set = []
             for human_exon in canonical_human_exons:
-                if huamn_exon in human_exon_map[para1].keys():
+                if human_exon in human_exon_map[para1].keys():
                     print human_exon
                     print  human_exon_map[para1][human_exon]
             if False:
