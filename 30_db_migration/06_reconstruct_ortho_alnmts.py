710c55135290c129aa9fa875a47ba393b5b0d106
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-01-07 14:17:16 +0100

    fusing seqs from differenst scaffolds

--- 30_db_migration/06_reconstruct_ortho_alnmts.py
+++ 30_db_migration/06_reconstruct_ortho_alnmts.py
@@ -1254,9 +1254,10 @@ def make_alignments ( gene_list, db_info):
             # we may have chosen to delete some sequences
             sorted_seq_names = sort_names (sorted_trivial_names['human'], output_pep)
 
-        print output_pep
-        print
-        print names_of_exons
+        for seq_name in output_pep.keys():
+            print seq_name
+            print "\t", 
+            print "\n\t".join(names_of_exons[seq_name])
         exit(1)
 
         if not check_seq_length (output_pep, "ouput_pep"): 
