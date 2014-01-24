1164b7c9ad25be0849e6437eea48438be2df2761
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-01-24 15:53:05 +0100

    housekeeping

--- 30_db_migration/06_reconstruct_ortho_alnmts.py
+++ 30_db_migration/06_reconstruct_ortho_alnmts.py
@@ -1354,7 +1354,10 @@ def make_alignments ( gene_list, db_info):
 
     # for each  gene in the provided list
     gene_ct = 0
-    gene_list.reverse()
+    #gene_list.reverse()
+    hack = gene_list[0:len(gene_list)/2] 
+    hack.reverse()
+    gene_list = hack
     for gene_id in gene_list:
 
         switch_to_db (cursor,  ensembl_db_name['homo_sapiens'])
