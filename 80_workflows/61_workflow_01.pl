c52b36a90ff0a6b8a7195e3247b1daad0abc8709
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-02-11 22:09:59 +0100

    fixing covered canonical exon problem

--- 80_workflows/61_workflow_01.pl
+++ 80_workflows/61_workflow_01.pl
@@ -7,7 +7,7 @@
 
 
 
-foreach (  "40_scratch_novel_exons.py",   "15_make_ortho_maps.py", 
+foreach (     "15_make_ortho_maps.py", 
 	   "18_find_missing_exons.py",    "20_novel_exon_cleanup.py",
 	   "17_ortho_exon_map_to_msa.py", "07_gene2exon_store.py",
 	   "21_make_novel_exon_maps.py",  "17_ortho_exon_map_to_msa.py",
@@ -16,12 +16,6 @@ foreach (  "40_scratch_novel_exons.py",   "15_make_ortho_maps.py",
     -e $_ || die "$_ not found.\n";
 }
 ($set, $no_threads, $method) =  @ARGV;
-
-=pod
-#40_scratch_novel_exons.py
-#$cmd = "40_scratch_novel_exons.py  $set  $no_threads ";
-#(system $cmd) && die "error running $cmd";
-=cut
 
 #07_gene2exon_store 
 $cmd = "07_gene2exon_store.py   $set  $no_threads ";
