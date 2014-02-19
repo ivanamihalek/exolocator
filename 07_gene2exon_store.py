34ff9c1bff6a8d3c23814c1b579174c23b4f7cdf
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-02-19 19:21:43 +0100

    combing gene2exon

--- 07_gene2exon_store.py
+++ 07_gene2exon_store.py
@@ -166,6 +166,7 @@ def get_translated_region(cursor, gene_id, species):
         qry += " FROM translation WHERE transcript_id=%d"  %  transcript_id
         rows = search_db (cursor, qry)
         if (not rows):
+            rows = search_db (cursor, qry, verbose=True)
             continue
         exon_seq_start = rows[0][0]
         start_exon_id  = rows[0][1]
