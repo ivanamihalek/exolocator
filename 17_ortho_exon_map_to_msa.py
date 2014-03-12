b11d9594ce05c6fa6e4836f62fecd5e507ec592a
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-03-12 21:37:14 +0100

    missing exons

--- 17_ortho_exon_map_to_msa.py
+++ 17_ortho_exon_map_to_msa.py
@@ -122,7 +122,6 @@ def multiple_exon_alnmt(gene_list, db_info):
                 seqname = "{0}:{1}:{2}".format(map.species_2, map.exon_id_2, exon_known_code)
                 headers.append(seqname)
                 sequences[seqname] = pepseq
-                print seqname, "  ", pepseq
 
  
             if (len(headers) <=1 ):
@@ -136,9 +135,6 @@ def multiple_exon_alnmt(gene_list, db_info):
             # align
             afa_fnm  = "{0}/{1}.afa".format( cfg.dir_path['scratch'], human_exon.exon_id)
             mafftcmd = acg.generate_mafft_command (fasta_fnm, afa_fnm)
-            print afa_fnm
-            continue
-
 
             #mafftcmd = "muscle -in" + fasta_fnm + " -out" + afa_fnm
             ret      = commands.getoutput(mafftcmd)
