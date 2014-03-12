ffd8c700b855e0859e080cf4fb6d8a9dde5a10f5
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-03-12 22:20:13 +0100

    missing exons

--- 12_seq_for_canonical_exons.py
+++ 12_seq_for_canonical_exons.py
@@ -272,8 +272,10 @@ def main():
 
     if local_db:
         db     = connect_to_mysql()
+        cfg    = ConfigurationReader()
     else:
         db     = connect_to_mysql(user="root", passwd="sqljupitersql", host="jupiter.private.bii", port=3307)
+        cfg    = ConfigurationReader       (user="root", passwd="sqljupitersql", host="jupiter.private.bii", port=3307)
     cursor = db.cursor()
     [all_species, ensembl_db_name] = get_species (cursor)
     print '======================================='
