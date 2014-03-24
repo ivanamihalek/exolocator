fca386bb8a496c3a900ae57df336fcd7269e6284
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-03-24 16:43:01 +0100

    reorganization

--- 30_db_migration/06_reconstruct_ortho_alnmts.py
+++ 30_db_migration/06_reconstruct_ortho_alnmts.py
@@ -665,6 +665,7 @@ def fix_one2many (cursor, ensembl_db_name, cfg, acg, sorted_seq_names, canonical
                 if alnmt_pep[human_exon].has_key(ortho_exon):
                     pep_seq_pieces.append( alnmt_pep[human_exon][ortho_exon].replace("-", "") )
                     break
+        print human_exons, ortho_exons
         print template_seq
         print pep_seq_pieces
         # pull  the slice out of the alignment
