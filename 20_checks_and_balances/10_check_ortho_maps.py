22804eda87e25281ac4e5307acc6cbc17564c17f
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-04-03 18:28:22 +0200

    housekeeping

--- 20_checks_and_balances/10_check_ortho_maps.py
+++ 20_checks_and_balances/10_check_ortho_maps.py
@@ -95,7 +95,7 @@ def main():
                     #if not species == 'pan_troglodytes': continue
                     exon               = map2exon(cursor, ensembl_db_name, map)
                     unaligned_sequence = get_exon_pepseq(cursor, exon, ensembl_db_name[species])
-                    if (1 or map.source=='sw_sharp'):
+                    if (0  and  map.source=='sw_sharp'):
                         print "\t", species,  map.source, map.exon_id_2, map.exon_known_2
                         print "\tmaps to ",  map.exon_id_1, map.exon_known_1
                         print "\tsim",  map.similarity
