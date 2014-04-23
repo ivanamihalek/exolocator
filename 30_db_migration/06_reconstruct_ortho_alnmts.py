85fdfc0d63529f205184110ad921de272cb86188
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-04-23 19:25:29 +0200

    ortho maps check

--- 30_db_migration/06_reconstruct_ortho_alnmts.py
+++ 30_db_migration/06_reconstruct_ortho_alnmts.py
@@ -1369,6 +1369,12 @@ def make_exon_alignments(cursor, ensembl_db_name, canonical_human_exons,
         [alnmt_pep[human_exon], alnmt_dna[human_exon]]  =   make_exon_alignment(cursor, ensembl_db_name,  human_exon.exon_id, 
                                                                                 human_exon.is_known,  mitochondrial, min_similarity, 
                                                                                 flank_length)   
+        print human_exon
+        for [name, seq] in alnmt_pep[human_exon].interitems():
+            print name
+            print seq
+        exit(1)
+
     return [alnmt_pep, alnmt_dna] 
 
 #########################################
