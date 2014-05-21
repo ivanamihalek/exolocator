532a60deed5ca5acb2408c49a50841fa7e3decf1
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-05-21 21:13:04 +0200

    getting rid of pseudogenes

--- 30_db_migration/06_reconstruct_ortho_alnmts.py
+++ 30_db_migration/06_reconstruct_ortho_alnmts.py
@@ -1320,6 +1320,7 @@ def remove_pseudogenes (cursor, ensembl_db_name, output_pep, sequence_name_to_ex
             # is there a sequence with >2 exons, and a 90% identical sequence with no exons?
             for para in paralogues: 
                 if para in single_exon: continue
+                print "single:", single, "para:", para
                 tanimoto = pairwise_tanimoto (output_pep[single], output_pep[para], use_heuristics=False)
                 if tanimoto>0.9: ###:  
                     # drop
