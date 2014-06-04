e4a6719fe3ae0cbbf5e6c667a916c28e8f8f90f4
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-06-04 13:27:39 +0200

    split exons

--- 10_ortho_exon_map_to_msa.py
+++ 10_ortho_exon_map_to_msa.py
@@ -181,7 +181,7 @@ def multiple_exon_alnmt(gene_list, db_info):
                 headers.append(seqname)
                 sequences[seqname] = pepseq
                 # for split exon concatenation (see below)
-                if not map.species_2 in exons_per_species.key():
+                if not map.species_2 in exons_per_species.keys():
                     exons_per_species[map.species_2] = []
                 exons_per_species[map.species_2].append ([exon_id_2, exon_known_code]);
                 
