a52001bce3bbe249ad2c49cb27cff49fc540d95c
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-06-10 15:47:16 +0200

    housekeepingsplit exons, still

--- 10_ortho_exon_map_to_msa.py
+++ 10_ortho_exon_map_to_msa.py
@@ -63,7 +63,7 @@ def concatenate_exons (cursor, ensembl_db_name, sequences, exons_per_species):
             overlap = False
             exon_prev = exons[0]
             for exon in exons[1:]:
-                 if exon_prev.pepseq_transl_end is None: 
+                if exon_prev.pepseq_transl_end is None: 
                     end_prev = exon_prev.end_in_gene
                 else:
                     end_prev = exon_prev.start_in_gene + exon_prev.pepseq_transl_end
