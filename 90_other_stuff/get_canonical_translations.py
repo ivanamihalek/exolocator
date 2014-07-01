52b55afa94dd58931d1233d53ecd3b7701f24d47
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-07-01 21:07:11 +0200

    side job

--- 90_other_stuff/get_canonical_translations.py
+++ 90_other_stuff/get_canonical_translations.py
@@ -19,12 +19,12 @@ def main():
     cursor = db.cursor()
     [all_species, ensembl_db_name] = get_species (cursor)
     species = 'homo_sapiens'
-    switch_to_db (cursor_species,  ensembl_db_name[species])
-    gene_list = get_gene_ids (cursor, species, biotype='protein_coding')
+    switch_to_db (cursor,  ensembl_db_name[species])
+    gene_list = get_gene_ids (cursor,  biotype='protein_coding')
 
     for gene_id in gene_list:
         # find stable
-        stable_id = gene2stable(cursor_species, gene_id=gene_id)
+        stable_id = gene2stable(cursor,  gene_id=gene_id)
         canonical = get_canonical_transl (acg, cursor, gene_id, species, strip_X = False)
         if canonical:
             print stable_id, canonical
