b1704c7f02c5ff66a83dd804d0c590dcc7e591ac
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-07-01 21:02:18 +0200

    side job

--- 90_other_stuff/get_canonical_translations.py
+++ 90_other_stuff/get_canonical_translations.py
@@ -8,20 +8,20 @@ from   el_utils.almt_cmd_generator import AlignmentCommandGenerator
 #########################################
 def main():
 
-    local_db = True
+    local_db = False
     if local_db:
         db     = connect_to_mysql()
         acg    = AlignmentCommandGenerator()
     else:
         db     = connect_to_mysql(user="root", passwd="sqljupitersql", host="jupiter.private.bii", port=3307)
         acg    = AlignmentCommandGenerator (user="root", passwd="sqljupitersql", host="jupiter.private.bii", port=3307)
-    print "=====================================  1 "
+
     cursor = db.cursor()
     [all_species, ensembl_db_name] = get_species (cursor)
     species = 'homo_sapiens'
     switch_to_db (cursor_species,  ensembl_db_name[species])
     gene_list = get_gene_ids (cursor_species, biotype='protein_coding')
-    print "=====================================  2 "
+
     for gene_id in gene_list:
         # find stable
         stable_id = gene2stable(cursor_species, gene_id=gene_id)
