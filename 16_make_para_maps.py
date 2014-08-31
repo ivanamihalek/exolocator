e73091e5c1ae97bcb6b844e35ddc6fba2057eda6
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-08-31 22:45:21 +0200

    housekeeping

--- 16_make_para_maps.py
+++ 16_make_para_maps.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 
 
 import StringIO
@@ -8,7 +8,7 @@ from random  import random, choice
 from   el_utils.mysql   import  *
 from   el_utils.ensembl import  *
 from   el_utils.utils   import  *
-from   el_utils.threads import  parallelize
+from   el_utils.processes import  parallelize
 from   el_utils.map     import  *
 from   el_utils.almt_cmd_generator import AlignmentCommandGenerator
 from   el_utils.config_reader      import ConfigurationReader
