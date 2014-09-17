1b4b1571c9846c7b9eb16e07e98cd9186fa7b7dc
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-09-17 16:50:21 +0200

    clearing up the flanking region/split codon issue(s)

--- 90_other_stuff/mutation_sig_bg_data.py
+++ 90_other_stuff/mutation_sig_bg_data.py
@@ -115,7 +115,7 @@ def main():
     # for each human gene
     #gene_ids = [10093176 ]
     gene_ct = 0
-    for gene_id in gene_ids[:3]:
+    for gene_id in gene_ids[:100]:
        
         switch_to_db (cursor,  ensembl_db_name['homo_sapiens'])
         stable_id = gene2stable(cursor, gene_id)
