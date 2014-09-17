5a45fca845c9e8f748b484ae86035cf7748b582f
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-09-17 15:06:09 +0200

    clearing up the flanking region/split codon issue(s)

--- 90_other_stuff/mutation_sig_bg_data.py
+++ 90_other_stuff/mutation_sig_bg_data.py
@@ -25,6 +25,55 @@ def align_nucseq_by_pepseq(aligned_pepseq, nucseq):
     aligned_nucseq = ''.join(('---' if c=='-' else next(codons) for c in aligned_pepseq))
     return aligned_nucseq
 
+
+
+
+category_dict = {}
+categories = []
+def fill_category ():
+    
+    categories = ["AT_tsvn", "AT_tstn", "CG_tsvn", "CG_tstn", "CpG_tsvn", "CpG_tstn"]
+    
+    category_dict['A']['A'][False] = ""
+    category_dict['A']['T'][False] = "AT_tsvn"
+    category_dict['A']['C'][False] = "AT_tsvn"
+    category_dict['A']['G'][False] = "AT_tstn"
+    
+    category_dict['T']['A'][False] = "AT_tsvn"
+    category_dict['T']['T'][False] = ""
+    category_dict['T']['C'][False] = "AT_tstn"
+    category_dict['T']['G'][False] = "AT_tsvn"
+    
+    category_dict['C']['A'][False] = "CG_tsvn"
+    category_dict['C']['T'][False] = "CG_tstn"
+    category_dict['C']['C'][False] = ""
+    category_dict['C']['G'][False] = "CG_tsvn"
+    
+    category_dict['C']['A'][True]  = "CpG_tsvn"
+    category_dict['C']['T'][True]  = "CpG_tstn"
+    category_dict['C']['C'][True]  = ""
+    category_dict['C']['G'][True]  = "CpG_tsvn"
+    
+    category_dict['G']['A'][False] = "CG_tstn"
+    category_dict['G']['T'][False] = "CG_tsvn"
+    category_dict['G']['C'][False] = "CG_tsvn"
+    category_dict['G']['G'][False] = ""
+    
+    category_dict['G']['A'][True] = "CpG_tstn"
+    category_dict['G']['T'][True] = "CpG_tsvn"
+    category_dict['G']['C'][True] = "CpG_tsvn"
+    category_dict['G']['G'][True] = ""
+#########################################
+def mutate(codon, mutation_position, new_nt):
+    mutated_codon = ""
+    for k in range(3):
+        if k==mutation_position:
+            mutated_codon.append(new_nt)
+        else:
+            mutated_codon.append(codon[k])
+    return mutated_codon
+   
+    
 #########################################
 def main():
 
@@ -44,14 +93,19 @@ def main():
     
     logf = erropen("error.log", "w") 
     if not logf: exit(1)
+    
+    outf = erropen("mut_significance_bg_data.txt", "w") 
+    if not outf: exit(1)
+
 
     switch_to_db (cursor, ensembl_db_name['homo_sapiens'])
     gene_ids = get_gene_ids (cursor, biotype='protein_coding', is_known=1, ref_only=True)
-
+    # the categories of mutations for which we will be collecting statistics
+    fill_category ()    
     # for each human gene
     #gene_ids = [10093176 ]
     gene_ct = 0
-    for gene_id in gene_ids[:100]:
+    for gene_id in gene_ids[:3]:
        
         switch_to_db (cursor,  ensembl_db_name['homo_sapiens'])
         stable_id = gene2stable(cursor, gene_id)
@@ -71,10 +125,6 @@ def main():
         for human_exon in canonical_human_exons:
             [exon_seq_id, pepseq, pepseq_transl_start, pepseq_transl_end, left_flank, right_flank, nucseq] = \
                     get_exon_seqs(cursor, human_exon.exon_id, human_exon.is_known)
-            print " %10d  %s " % (human_exon.exon_id, pepseq)
-            print "lengths:  %4d  %4d " % (len(pepseq)*3, len(nucseq[pepseq_transl_start:pepseq_transl_end]))
-            # HERE: IN MY CASE pepseq_transl_end should be 1, not 0
-            print "pepseq_transl_start pepseq_transl_end ",  pepseq_transl_start, pepseq_transl_end, " exon len", len(nucseq)
             # add the split codon
             phase = get_exon_phase (cursor, human_exon.exon_id, human_exon.is_known)
             left_flank_plus_codon_piece = left_flank + nucseq[:pepseq_transl_start]
@@ -84,12 +134,11 @@ def main():
                 # hedge against the possibility that the translation starts
                 # right at the start of the exon, but there is supposed to be a phase
                 split_codon = prev_codon_piece_plus_right_flank[:phase] + left_flank_plus_codon_piece[-offset:]
-            print "split_codon:", split_codon            
             full_reconstituted_cDNA += split_codon + nucseq[pepseq_transl_start:pepseq_transl_end]
             prev_codon_piece_plus_right_flank = nucseq[pepseq_transl_end:] + right_flank
             
-            
-        if ( is_mitochondrial(cursor, gene_id)):
+        mitochondrial = is_mitochondrial(cursor, gene_id);
+        if (mitochondrial):
             full_reconstituted_seq = Seq(full_reconstituted_cDNA).translate(table="Vertebrate Mitochondrial").tostring()
         else:
             full_reconstituted_seq = Seq(full_reconstituted_cDNA).translate().tostring()
@@ -105,14 +154,73 @@ def main():
             print >> logf, full_reconstituted_seq
             continue
 
-        print gene_id, stable_id, get_description (cursor, gene_id)
+        # nucleotide stats
+        count = {'A':0, 'C':0, 'C-CpG':0, 'T':0, 'G':0, 'G-CpG':0} 
         codons = map(''.join, zip(*[iter(full_reconstituted_cDNA)]*3))
+        L = len(full_reconstituted_cDNA)
+        is_CpG = []
+        for i in range(L):
+            is_CpG[i] = False
+            if full_reconstituted_cDNA[i] == 'A':
+                count['A'] += 1
+            elif full_reconstituted_cDNA[i] == 'T':
+                count['T'] += 1
+            elif full_reconstituted_cDNA[i] == 'C':
+                if i + 1 < L and full_reconstituted_cDNA[i + 1] == 'G':
+                    count['C-CpG'] += 1
+                    is_CpG[i] = True
+                else:
+                    count['C'] += 1
+            elif full_reconstituted_cDNA[i] == 'G':
+                if i > 0 and full_reconstituted_cDNA[i - 1] == 'C':
+                    count['G-CpG'] += 1
+                    is_CpG[i] = True
+                else:
+                    count['G'] += 1
+                    
+        # in each category_dict (AT transt, AT transv, CG trans, CG transv, Cpg trans, cpGtransv, how many missense, 
+        #  how many nonsense, how many silent  possible    
+        silent   = {}
+        missense = {}
+        nonsense = {}
+        for cg in categories:
+            silent[cg] = 0
+            missense[cg] = 0
+            nonsense[cg] = 0
         for i in range(len(codons)):
-            print " %5d  %s  %s " % (i, full_reconstituted_seq[i], codons[i])
-        print  "canonical:"
-        print canonical
-        print "reconstituted:"
-        print full_reconstituted_seq
+            codon = codons[i]
+            aa = full_reconstituted_seq[i]
+            for j in range(3):
+                nt_position = i*3 + j
+                nt = full_reconstituted_seq[nt_position]
+                for new_nt in ['A', 'C', 'T', 'G']:
+                    if new_nt == nt: continue
+                    mutated_codon = mutate(codon, j, new_nt)
+                    if (mitochondrial):
+                        mutated_aa = Seq(mutated_codon).translate(table="Vertebrate Mitochondrial").tostring()
+                    else:
+                        mutated_aa = Seq(mutated_codon).translate().tostring()
+                        
+                    cg = category_dict[codon[j]][new_nt][is_CpG[nt_position]];
+                    if (mutated_aa == aa):
+                        silent[cg] += 1
+                    elif (mutated_aa == "*"):
+                        nonsense[cg] += 1
+                    else:
+                        missense[cg] += 1
+                
+        print stable_id, get_description (cursor, gene_id)
+        for i in range(len(codons)):
+            if (is_CpG[i]):
+                print " %5d  %s  %s " % (i, full_reconstituted_seq[i], codons[i])
+        print "%10s  %5s  %5s  %5s" % ("category", "silent", "nonsense", "missense")
+        for cg in categories:
+            print "%10s  %5d  %5d  %5d" %(cg, silent[cg], nonsense[cg], missense[cg])
+        print "done", stable_id
+        #print  "canonical:"
+        #print canonical
+        #print "reconstituted:"
+        #print full_reconstituted_seq
         
 
     logf.close()
