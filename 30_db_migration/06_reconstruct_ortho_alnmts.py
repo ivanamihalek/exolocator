4e0f47fb69db2bd6587bf7f212b7a6f27ab4b0a7
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-10-08 20:29:04 +0200

    housekeeping

--- 30_db_migration/06_reconstruct_ortho_alnmts.py
+++ 30_db_migration/06_reconstruct_ortho_alnmts.py
@@ -934,12 +934,8 @@ def fix_split_codons (cursor, ensembl_db_name, cfg, acg, sorted_seq_names,
                 left_flank_plus_codon_piece = exon_left_flank.lower() + exon_dna_seq[:trsl_from]
                 flanking_nucleotides   = left_flank_plus_codon_piece [-offset:]
                 codon                  = cary + flanking_nucleotides
-                print cary, flanking_nucleotides, codon
                 retval = translate (codon, 0, mitochondrial, strip_stop = False)
-
-                print cary, flanking_nucleotides, codon, retval
-
-                [phase_suggested, res] = retval
+                [offset, length_translated,  res, phase_suggested] = retval
 
                 if res: # we are going to have an insert here
                     insert [name][prev_local_pos+3] = res
