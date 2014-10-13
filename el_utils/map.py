dd1ae3290a4da671603b96bdfa25442618460cdd
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-10-13 17:01:38 +0200

    hosekeeping

--- el_utils/map.py
+++ el_utils/map.py
@@ -72,8 +72,8 @@ def get_maps(cursor, ensembl_db_name, exon_id, is_known, species = 'homo_sapiens
     maps = []
 
     switch_to_db (cursor,  ensembl_db_name[species])
-    if ( type(exon_id) != int):
-        print "exon_id ", exon_id
+    if not isinstance(exon_id, int):
+        print ">>>>>>>>>>>   exon_id ", exon_id
         exit(1)
     qry  = "select * from %s where exon_id = %d " % (table, exon_id)
     qry += " and exon_known = %d " % is_known
