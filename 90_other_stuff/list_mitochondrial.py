5fafebc7564b21ca5da7b11848822e400dcba7d2
Ivana Mihalek <ivana.mihalek@gmail.com>
2014-10-17 20:00:33 +0200

    house

--- 90_other_stuff/list_mitochondrial.py
+++ 90_other_stuff/list_mitochondrial.py
@@ -22,7 +22,7 @@ def main():
     switch_to_db (cursor,  ensembl_db_name[species])
     gene_ids = get_gene_ids (cursor, biotype='protein_coding', is_known=1, ref_only=True)
     for gene_id in gene_ids:
-        if not is_mitochondrial(cursor, human_gene_id): continue
+        if not is_mitochondrial(cursor, gene_id): continue
         print gene2stable(cursor, gene_id), get_description(cursor, gene_id)
         
 
