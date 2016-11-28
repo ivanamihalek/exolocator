1a8ed9c4e741f8dc49e2ba27cb5d421724dedc38
ivana <ivana.mihalek@gmail.com>
2016-11-28 15:36:38 -0500

    compara

--- 00_download_dbs/04_make_ncbi_tax_db.pl
+++ 00_download_dbs/04_make_ncbi_tax_db.pl
@@ -1,8 +1,6 @@
 #!/usr/bin/perl -w
 
-$path_to_db = "/home/ensembl/ncbi_tax";
-
-$credentials = "-u root -psqljupitersql -S /var/run/mysqld/ensembl.sock";
+$path_to_db = "/databases/ensembl/ncbi_tax";
 
 
 (-e $path_to_db) || die "$path_to_db not found\n";
@@ -19,7 +17,7 @@ foreach $db  ("ncbi_taxonomy") {
     chdir "$path_to_db/$db";
 
     print "making db  ... \n";
-    $cmd  = "mysqladmin $credentials  create $db";
+    $cmd  = "mysqladmin  --login-path=client create $db";
  
     print "\t $cmd\n";
     (system $cmd) && warn "error running $cmd\n";
@@ -27,13 +25,15 @@ foreach $db  ("ncbi_taxonomy") {
 
     $sqlname = "$db.sql";
     (-e $sqlname) || die "$sqlname not found in $path_to_db/$db";
-    $cmd = "mysql $credentials    $db < $sqlname";
+    $cmd = "mysql  --login-path=client   $db < $sqlname";
     (system $cmd) && warn "error running $cmd\n";
-=pod
-mysqlimport loads tables from text files in various formats.  The base name of the
-text file must be the name of the table that should be used.
-=cut
-    $cmd = "mysqlimport  $credentials   ".
+
+    =pod
+    mysqlimport loads tables from text files in various formats.  The base name of the
+    text file must be the name of the table that should be used.
+    =cut
+
+    $cmd = "mysqlimport --login-path=client  ".
 	"--fields_terminated_by=\'\\t|\\t\'  ".
 	"--lines_terminated_by=\'\\t|\\n'\  ".
 	"$db -L *.dmp";
